e5981c6d6c8e1db1c58a883e35c1a07a3d1c5ea4
simonewebdesign <hello@simonewebdesign.it>
2015-07-11 19:24:24 +0100

    Add chat room (Wip)

---
+++ lib/chat_room.ex
@@ -0,0 +1,36 @@
+defmodule ChatRoom do
+  @moduledoc """ A chat room.
+  
+  ## API reference:
+
+      enter(name)
+      leave(name)
+      message(name, message)
+  """
+
+  use GenServer
+
+  ## Client API
+
+  @doc """ 
+  Enter in a room.
+  """
+  def enter(name) do
+    
+  end
+
+  @doc """
+  Leave a room.
+  """
+  def leave(name) do
+    
+  end
+
+  @doc """
+  Send a message in a room.
+  """
+  def message(name, message) do
+    
+  end
+
+end
